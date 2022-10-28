d94f4f95fa43162f00a64c8e1c98186e11b5dd97
Esdras Albino <esdras.albino@nave.org.br>
2022-10-27 21:07:40 -0300

    final

--- script/app.js
+++ script/app.js
@@ -3,6 +3,7 @@ const section = document.querySelector(".section");
 
 
 window.addEventListener("DOMContentLoaded", e => {
+    e.preventDefault();
 
     fetch('https://dummyjson.com/users')
             .then(res => res.json())
@@ -89,18 +90,27 @@ window.addEventListener("DOMContentLoaded", e => {
                         color: black;
                     `
 
+                    div1.addEventListener('click', e => {
+                        sessionStorage.setItem('dados', img.getAttribute('src'));
+                    })
+
                     div1.appendChild(div)
                 })
     }
 
+    const imgs = ['https://ciclovivo.com.br/wp-content/uploads/2018/10/iStock-486755202-768x512.jpg', 'https://ciclovivo.com.br/wp-content/uploads/2018/10/iStock-687112264-768x479.jpg',
+    'https://ciclovivo.com.br/wp-content/uploads/2018/10/iStock-869138876-768x513.jpg', 'https://ciclovivo.com.br/wp-content/uploads/2018/10/iStock-694634868-768x512.jpg', 
+    'https://ciclovivo.com.br/wp-content/uploads/2018/10/iStock-601901056-768x517.jpg','https://ciclovivo.com.br/wp-content/uploads/2018/10/iStock-178390705-768x512.jpg',
+    'https://ciclovivo.com.br/wp-content/uploads/2018/10/iStock-646190144-768x510.jpg','https://ciclovivo.com.br/wp-content/uploads/2018/10/iStock-628615976-768x512.jpg']
 
-    for(let i = 0; i < 30; i++){
+    for(let i = 0; i < 15; i++){
 
         fetch('https://dummyjson.com/posts')
             .then(res => res.json())
             .then(res => {
                 let random = Math.floor(Math.random() * res.posts.length); 
                 const post = res.posts[random];
+                let randomImgs = Math.floor(Math.random() * imgs.length)
                 
                 const a = document.createElement("a");
                 const img = document.createElement("img");
@@ -110,7 +120,7 @@ window.addEventListener("DOMContentLoaded", e => {
                 const div = document.createElement("div");
         
                 user(img);
-                imgPost.src = "https://imagens.mdig.com.br/thbs/45184mn.jpg";
+                imgPost.src = imgs[randomImgs];
         
                 a.href = "usuario.html";
                 p.textContent = post.body;
@@ -139,7 +149,10 @@ window.addEventListener("DOMContentLoaded", e => {
                     box-shadow: 3px 5px 10px rgba(0, 0, 0, 0.315);
                     margin: 20px;
                 `
-        
+                
+                img.addEventListener("click", e =>{
+                    sessionStorage.setItem('dados', img.getAttribute('src'));
+                })
                 a.appendChild(img);
                 div.appendChild(a);
                 div.appendChild(imgPost);
